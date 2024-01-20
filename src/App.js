3716a8adda72d0abc989dbc0ffe6f8f81e483ce3
vishu bhandari <deevishu1234@gmail.com>
2024-01-20 23:24:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
-// import AddIcon from '@mui/icons-material/Add';
-// import DeleteIcon from '@mui/icons-material/Delete';
+import PlusOneIcon from '@mui/icons-material/PlusOne';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
 
 const App = () => {
   const [num, setNum] = useState(0);
@@ -29,14 +30,14 @@ const App = () => {
         <div className="center_div">
           <h1>{num}</h1>
           <div className="btn_div">
-            <button className="increment_btn" onClick={incNum}>
-            {/* <AddIcon/> */}
+            <Button variant="outlined" className="increment_btn" onClick={incNum}>
             Increment
-            </button>
-            <button className="decrement_btn" onClick={decNum}>
-              {/* <DeleteIcon/> */}
+            <PlusOneIcon/> 
+            </Button>
+            <Button variant="outlined" className="decrement_btn" onClick={decNum}>
               Decrement
-            </button>
+              <DeleteIcon/> 
+            </Button>
           </div>
           <div id="alert" className="alert">
             Limit Reached
