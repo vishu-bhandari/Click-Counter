0e4a64a203f72090873b078bcf6f2949d3dc7bbb
vishu bhandari <deevishu1234@gmail.com>
2024-01-20 15:46:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import "./App.css"; // Import your CSS file
+import "./App.css";
 
 const App = () => {
+  const [num, setNum] = useState(0);
 
-  const [num,setNum]=useState(0);
+  const incNum = () => {
+    setNum(num + 1);
+  };
 
-  const incNum=()=>{
-    setNum(num +1);
-  }
-  const decNum=()=>{
-    setNum(num-1);
-    if(num<0){
-      alert("your num is less than 0")
-      return;
+  const decNum = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    } else {
+      document.getElementById("alert").style.display = "block";
+      setTimeout(() => {
+        document.getElementById("alert").style.display = "none";
+      }, 2000);
+      setNum(0);
     }
-  }
-
+  };
 
   return (
     <>
+      <h1>Click Counter</h1>
       <div className="main_div">
         <div className="center_div">
           <h1>{num}</h1>
           <div className="btn_div">
-            <button className="increment_btn" onClick={incNum}>Increment</button>
-            <button className="decrement_btn" onClick={decNum}>Decrement</button>
+            <button className="increment_btn" onClick={incNum}>
+              Increment
+            </button>
+            <button className="decrement_btn" onClick={decNum}>
+              Decrement
+            </button>
+          </div>
+          <div id="alert" className="alert">
+            Limit Reached
           </div>
         </div>
       </div>
