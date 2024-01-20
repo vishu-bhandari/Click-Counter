75167d1c620ed1265180658e3659af15ed01c512
vishu bhandari <deevishu1234@gmail.com>
2024-01-20 15:33:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"; // Import your CSS file
 
-function App() {
+const App = () => {
+
+  const [num,setNum]=useState(0);
+
+  const incNum=()=>{
+    setNum(num +1);
+  }
+  const decNum=()=>{
+    setNum(num-1);
+    if(num<0){
+      alert("your num is less than 0")
+      return;
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{num}</h1>
+          <div className="btn_div">
+            <button className="increment_btn" onClick={incNum}>Increment</button>
+            <button className="decrement_btn" onClick={decNum}>Decrement</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
